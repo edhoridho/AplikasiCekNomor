70abd763cf3c0711c3dfbf574a96d5c9d124b3ea
dho <edridho.dev@gmail.com>
2024-11-10 22:57:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AplikasiCekNomor
+ Muhammad Ridho - 2210010618 - Aplikasi Cek Nomor
---
+++ src/aplikasiceknomor/AplikasiCekNomor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasiceknomor;
+
+/**
+ *
+ * @author ACER A314
+ */
+public class AplikasiCekNomor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
